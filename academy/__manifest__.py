6187a02a2afc5c0928e80f6d62be2ddee66b6480
MIGUEL VILLADIEGO GARCIA <44074942+vmvilladiego@users.noreply.github.com>
2023-07-30 16:02:04 -0500

    Add files via upload

---
+++ academy/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Odoo Academy',
+    'version' : '1.0',
+    'summary': 'Module to handle Course and Sessions',
+    'description': """
+        Module to Handle
+        ====================
+        - Courses
+        - Sessions
+        - Attendees
+            """,
+    'license': 'LGPL-3',
+    'author': 'MIGUEL VILLADIEGO GARCIA',
+    'website': 'vmvg.com',
+    'category': 'Custom Modules/Tech Training',
+    'depends' : ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
