6512643a70c77bcb59d54d1dd59899b6816106d6
vmvilladiego <miguel.villadiego@gmail.com>
2023-07-31 01:12:32 +0000

    [ADD] Course Models Update

--- academy/models/course.py
+++ academy/models/course.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
+from odoo import fields, models
 
 class Course(models.Model):
     _name = 'academy.course'
     _description = 'Course Information'
+
+    name = fields.Char(string="Title", required=True)
+    active = fields.Boolean(string="Active", default=True)
+
+    description = fields.Text()
+    level = fields.Selection(string="Level",
+                             selection=[
+                                 ('beginner','Beginner'),
+                                 ('intermediate','Intermediate'),
+                                 ('advance','Advance'),
+                             ],
+                             copy=False)
+    
